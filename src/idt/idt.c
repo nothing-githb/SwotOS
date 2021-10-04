cf01cf1ff0da9ce573c3a54143b76b31d6c31ae1
nothing-githb <sahinh20@gmail.com>
2021-10-04 23:25:08 +0300

    Programmable interrupt controller implemented

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -2,10 +2,25 @@
 #include "config.h"
 #include "kernel.h"
 #include "memory/memory.h"
+#include "io/io.h"
+
 struct idt_desc idt_descriptors[SWOTOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
 extern void idt_load(struct idtr_desc* ptr);
+extern void int21h();
+extern void no_interrupt();
+
+void int21h_handler()
+{
+    print("Key pressed!\n");
+    outb(0x20, 0x20);
+}
+
+void no_interrupt_handler()
+{
+    outb(0x20, 0x20);
+}
 
 void idt_zero()
 {
@@ -24,11 +39,17 @@ void idt_set(int interrupt_no, void* address)
 
 void idt_init()
 {
+    int i;
     memset(idt_descriptors, 0, sizeof(idt_descriptors));
     idtr_descriptor.limit = sizeof(idt_descriptors) -1;
     idtr_descriptor.base = (uint32_t) idt_descriptors;
 
+    for (i = 0; i < SWOTOS_TOTAL_INTERRUPTS; i++)
+    {
+        idt_set(i, no_interrupt);
+    }
     idt_set(0, idt_zero);
+    idt_set(0x21, int21h);
 
     // Load the interrupt descriptor table
     idt_load(&idtr_descriptor);
--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "idt/idt.h"
+#include "io/io.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -75,7 +76,7 @@ void kernel_main()
     print("Hello world!\ntest");
 
     // Initialize the interrupt descriptor table
-
     idt_init();
+    
 
 }
